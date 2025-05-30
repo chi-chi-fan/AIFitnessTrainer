7d74955981199eaf255f1be7e3f9dfd89a9b98ef
chi-chi-fan <free0nemeng@gmail.com>
2025-05-30 08:54:46 -0700

    auth

--- fitnesstrainer/src/app/page.tsx
+++ fitnesstrainer/src/app/page.tsx
@@ -1,9 +1,18 @@
+"use client";3
+import { SignedIn, SignedOut, SignInButton } from '@clerk/clerk-react'
+import { SignOutButton } from '@clerk/nextjs'
 import React from 'react'
 
 const HomePage = () => {
   return (
     <div>
       HomePage
+      <SignedOut>
+        <SignInButton />
+      </SignedOut>
+      <SignedIn>
+        <SignOutButton />
+      </SignedIn>
     </div>
   )
 }
